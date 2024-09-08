a299a2224bacad24ab49196c857a4c6a07752a6e
samiyak14 <saamia.kb@gmail.com>
2024-09-08 18:58:31 +0300

    hello name

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hello World</h1>'
+
+@app.route('/home/<name>',methods=['GET','POST'],defaults={'name':'Default'})
+@app.route('/home/<name>',methods=['GET','POST'])
+def home(name) :
+    return '<h1>hii {} welcome to this website!</h1>'.format(name)
 
 if __name__=='__main__':
     app.run()
